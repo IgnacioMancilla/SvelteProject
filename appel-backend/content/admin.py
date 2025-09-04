070370fa6213c06ee16a04879d1899abb3faac0e
IgnacioMancilla <167275097+IgnacioMancilla@users.noreply.github.com>
2025-09-03 20:21:35 -0400

    Delete appel-backend directory

--- appel-backend/content/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Slide
-
-@admin.register(Slide)
-class SlideAdmin(admin.ModelAdmin):
-    list_display = ('id','alt','order','is_active','updated')
-    list_editable = ('order','is_active')
-    search_fields = ('alt','caption')
